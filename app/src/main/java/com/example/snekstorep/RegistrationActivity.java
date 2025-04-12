d09fb4c215f63a5d6db291e6dc34ebedb518a2a6
Chris Patrick <cchilonsegura@gmail.com>
2025-04-12 15:52:57 -0500

    Login y registro layout

--- app/src/main/java/com/example/snekstorep/RegistrationActivity.java
+++ app/src/main/java/com/example/snekstorep/RegistrationActivity.java
@@ -1,6 +1,8 @@
 package com.example.snekstorep;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,20 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    //Chris
-
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void signup(View view) {
+        startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+    }
+    public void singin(View view) {
+        startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+    }
+
+
 }
