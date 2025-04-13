892a72e26ff7c6ee599e6eb96c45b3f7b053910f
Chris Patrick <cchilonsegura@gmail.com>
2025-04-13 14:10:38 -0500

    Login & Register - Firebase Implementation

--- app/src/main/java/com/example/snekstorep/RegistrationActivity.java
+++ app/src/main/java/com/example/snekstorep/RegistrationActivity.java
@@ -8,14 +8,21 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     EditText name,email,password;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +35,17 @@ public class RegistrationActivity extends AppCompatActivity {
             return insets;
         });
 
+        //getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser()!=null){
+            startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            finish();
+        }
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
-
-
 
     }
 
@@ -43,22 +56,34 @@ public class RegistrationActivity extends AppCompatActivity {
         String userPassword = password.getText().toString();
 
         if (TextUtils.isEmpty(userName)) {
-            Toast.makeText( this,  "Enter Name!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "¡Ingresa un nombre!", Toast.LENGTH_SHORT).show();
         }
 
         if (TextUtils.isEmpty(userEmail)) {
-            Toast.makeText( this,  "Enter Email Address!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "¡Ingresa una dirección de correo!", Toast.LENGTH_SHORT).show();
             return;
         }
 
         if (TextUtils.isEmpty(userPassword)) {
-            Toast.makeText( this,  "Enter Password!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "¡Ingresa una contraseña!", Toast.LENGTH_SHORT).show();
         }
-        if (userPassword.length() < 6){
-            Toast.makeText( this,  "password too short, enter ninimun 6 characters!", Toast.LENGTH_SHORT).show();
+        if (userPassword.length() < 6) {
+            Toast.makeText(this, "¡Contraseña muy corta, ingresa mínimo 6 caracteres!", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        auth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegistrationActivity.this, "Successfully Register",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                        }else {
+                            Toast.makeText(RegistrationActivity.this, "Registration Failed"+ task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
     public void signin(View view) {
