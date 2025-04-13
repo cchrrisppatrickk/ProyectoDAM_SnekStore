13f95d2b01bb21485a4540eae3ccf32911184245
Chris Patrick <cchilonsegura@gmail.com>
2025-04-13 12:29:02 -0500

    Login y registro layout-Fix

--- app/src/main/java/com/example/snekstorep/LoginActivity.java
+++ app/src/main/java/com/example/snekstorep/LoginActivity.java
@@ -29,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(new Intent(LoginActivity.this, MainActivity.class));
     }
 
-    public void singUp(View view) {
+    public void signUp(View view) {
         startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
     }
 }
--- app/src/main/java/com/example/snekstorep/RegistrationActivity.java
+++ app/src/main/java/com/example/snekstorep/RegistrationActivity.java
@@ -2,7 +2,10 @@ package com.example.snekstorep;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    EditText name,email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +27,41 @@ public class RegistrationActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+
+
     }
 
     public void signup(View view) {
-        startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if (TextUtils.isEmpty(userName)) {
+            Toast.makeText( this,  "Enter Name!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (TextUtils.isEmpty(userEmail)) {
+            Toast.makeText( this,  "Enter Email Address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(userPassword)) {
+            Toast.makeText( this,  "Enter Password!", Toast.LENGTH_SHORT).show();
+        }
+        if (userPassword.length() < 6){
+            Toast.makeText( this,  "password too short, enter ninimun 6 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
     }
-    public void singin(View view) {
+    public void signin(View view) {
         startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
     }
 
